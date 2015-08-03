345c263b8d426543d74c48e4ebc0a8447bb6c1ad
Demo User <demouser@MacBook-Air.local>
2015-08-03 11:27:51 -0400

    adding main.py

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MakeEvent(ndb.Model):
+class Event(ndb.Model):
     db_firstname = ndb.StringProperty(required=True)
     db_lastname = ndb.StringProperty(required=True)
     db_eventname = ndb.StringProperty(required=True)
     db_description = ndb.StringProperty(required=True)
+    db_start_time = ndb.StringProperty(required=True)
+    db_end_time = ndb.StringProperty(required=True)
     db_image = ndb.BlobProperty()
 
 class HomePage(webapp2.RequestHandler):
@@ -24,6 +26,11 @@ class EventMaker(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('make_event.html')
         self.response.write(template.render())
+    # def post(self):
+    #     redirect
+# class OneEvent(webapp2.RequestHandler):
+#     def get(self):
+
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
