66c668b2a9b4c6ee964c4e4d62f39e99f2b743d4
Jolines <jolines.contreras@gmail.com>
2015-08-05 17:13:18 -0400

    added js

---
+++ animations.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#searchtext').animate(
+      {fontSize: 24},
+      1500
+  );
+});
+
+
+
+
+// 	$('.find').jqFloat({
+// 		width: 100,
+// 		height: 100,
+// 		speed: 1000
+// 	});
+// });
