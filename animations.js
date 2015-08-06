0fac9b77ac8d30cd00b1e15e95c2041bc351a437
Jolines <jolines.contreras@gmail.com>
2015-08-06 00:52:41 -0400

    animations

--- animations.js
+++ animations.js
@@ -3,14 +3,29 @@ $(document).ready(function() {
       {fontSize: 24},
       1500
   );
+
+// $(".find").animate({width:'toggle'},350);
+
+$(".find").hide();
+$(".find").slideToggle(1000);
+
+$(".create").hide();
+$(".create").slideToggle(1000);
+
+$("#txt").hide();
+$("#txt").slideDown(1000);
+
+
+	$('.cloud').jqFloat({
+		width: 100,
+		height: 100,
+		speed: 5000
+	});
+
+  // $('#createimage').jqFloat({
+	// 	width: 100,
+	// 	height: 100,
+	// 	speed: 1000
+	// });
+
 });
-
-
-
-
-// 	$('.find').jqFloat({
-// 		width: 100,
-// 		height: 100,
-// 		speed: 1000
-// 	});
-// });
